7081322512122a6740c9d654f98f494a88a9c347
barneygale <barney.gale@gmail.com>
2024-06-08 18:05:09 +0100

    Error handling, richer stat objects.

--- pathlib_artifactory.py
+++ pathlib_artifactory.py
@@ -1,18 +1,57 @@
-import dataclasses
+from dataclasses import dataclass
+from datetime import datetime
 import errno
 import io
 import posixpath
 import stat
+from typing import Optional
 
 from requests import Session
 from pathlib_abc import PathBase, UnsupportedOperation
 
 
-@dataclasses.dataclass
+def _parse_datetime(date_str):
+    if date_str.endswith('Z'):
+        date_str = date_str[:-1] + '+00:00'
+    return datetime.fromisoformat(date_str)
+
+
+@dataclass
 class ArtifactoryStat:
-    st_mode: int
-    st_size: int
-    st_children: list
+    created: datetime
+    modified: datetime
+    created_by: str
+    modified_by: str
+    size: int
+    is_dir: bool
+    children: Optional[list] = None
+    md5: Optional[str] = None
+    sha1: Optional[str] = None
+    sha256: Optional[str] = None
+    mime_type: Optional[str] = None
+
+    @classmethod
+    def from_storage_response(cls, data):
+        checksums = data.get('checksums', {})
+        children = None
+        if 'children' in data:
+            children = [child['uri'][1:] for child in data['children']]
+        return cls(
+            created=_parse_datetime(data['created']),
+            modified=_parse_datetime(data['lastModified']),
+            created_by=data['createdBy'],
+            modified_by=data['modifiedBy'],
+            size=int(data.get('size', 0)),
+            is_dir='size' not in data,
+            children=children,
+            md5=checksums.get('md5'),
+            sha1=checksums.get('sha1'),
+            sha256=checksums.get('sha256'),
+            mime_type=data.get('mimeType'))
+
+    @property
+    def st_mode(self):
+        return stat.S_IFDIR if self.is_dir else stat.S_IFREG
 
 
 class ArtifactoryPath(PathBase):
@@ -24,7 +63,7 @@ class ArtifactoryPath(PathBase):
         self.session = session or Session()
 
     def __repr__(self):
-        return f"{type(self).__name__}({str(self)!r}, base_uri={self.base_uri!r})"
+        return f'{type(self).__name__}({str(self)!r}, base_uri={self.base_uri!r})'
 
     def __hash__(self):
         return hash((self.base_uri, str(self)))
@@ -38,27 +77,27 @@ class ArtifactoryPath(PathBase):
         return type(self)(*pathsegments, base_uri=self.base_uri, session=self.session)
 
     def stat(self, *, follow_symlinks=True):
-        uri = self.base_uri + "/api/storage" + str(self)
+        uri = f'{self.base_uri}/api/storage{self}'
         response = self.session.get(uri)
         if response.status_code == 404:
-            raise OSError(errno.ENOENT, "File not found", str(self))
+            raise OSError(errno.ENOENT, 'File not found', str(self))
         response.raise_for_status()
-        response = response.json()
-        if 'size' in response:
-            return ArtifactoryStat(stat.S_IFREG, int(response['size']), [])
-        else:
-            return ArtifactoryStat(stat.S_IFDIR, 0, response['children'])
+        return ArtifactoryStat.from_storage_response(response.json())
 
     def open(self, mode='r', buffering=-1, encoding=None,
              errors=None, newline=None):
         if buffering != -1:
             raise UnsupportedOperation(f'Unsupported buffering: {buffering!r}')
-        uri = f'{self.base_uri}{self}'
+
         action = ''.join(c for c in mode if c not in 'btU')
         if action == 'r':
+            st = self.stat()
+            if st.is_dir:
+                raise OSError(errno.EISDIR, 'Is a directory', str(self))
+            uri = f'{self.base_uri}{self}'
             response = self.session.get(uri, stream=True)
             if response.status_code == 404:
-                raise OSError(errno.ENOENT, "File not found", str(self))
+                raise OSError(errno.ENOENT, 'File not found', str(self))
             response.raise_for_status()
             fileobj = response.raw
         else:
@@ -70,8 +109,9 @@ class ArtifactoryPath(PathBase):
 
     def iterdir(self):
         st = self.stat()
-        for child in st.st_children:
-            yield self.joinpath(child['uri'][1:])
+        if not st.is_dir:
+            raise OSError(errno.ENOTDIR, 'Not a directory', str(self))
+        return iter([self.joinpath(name) for name in st.children])
 
     def absolute(self):
         if self.is_absolute():
